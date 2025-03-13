5f8db2563e484a9cf19fa61627d10aa57bf401a3
Ricardo Lima <limaricardop@gmail.com>
2025-03-13 16:18:15 -0300

    fix: deploy

--- sigecont-web/src/api/config.ts
+++ sigecont-web/src/api/config.ts
@@ -5,5 +5,5 @@ const API_URL = import.meta.env.VITE_API_URL || "https://localhost:7123";
 export const client = axios.create({
     baseURL: API_URL,
     timeout: 30000,
-    withCredentials: false
+    withCredentials: true
 });
