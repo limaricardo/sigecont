c72ac7da22edace143dfd886872d59b6d7f2e888
Ricardo Lima <limaricardop@gmail.com>
2025-03-13 14:25:00 -0300

    fix: deploy front

--- sigecont-web/src/api/config.ts
+++ sigecont-web/src/api/config.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "https://localhost:7123";
+const API_URL = import.meta.env.VITE_API_URL || "https://localhost:7123";
 
 export const client = axios.create({
     baseURL: API_URL,
